930a0d601cda31b6b4d3e478b20e634683e2a1cf
09x09 <teemengtan@gmail.com>
2019-04-26 12:16:13 +0800

    IT WORKS

--- ROS/trolley/src/localisation_test.py
+++ ROS/trolley/src/localisation_test.py
@@ -3,53 +3,54 @@ import rospy
 import pathplanner
 
 from geometry_msgs.msg import PoseStamped
-
-def gripper_test(bot):
-	rospy.loginfo("Gripper Test")
-	rospy.loginfo("Lower gripper")
-	bot.gripper_down()
-	rospy.sleep(5)
-	rospy.loginfo("Raise gripper")
-	bot.gripper_up()
-	rospy.loginfo("Gripper test complete")
-
-def localisation_test(bot, vl, vr):	
-	vell, velr = vl, vr
-	counter = 1	
-	test_x = 1
-	test_time = 10
-	while counter < test_time:
-		bot.vel_publish(vell,velr)
-		bot.ekf_sub()
-		counter +=1
-	bot.vel_publish(0,0)
+from std_msgs.msg import Int32
 
 		
 def cb(msg):
-	rospy.loginfo("received")
+	data = msg.data
+	rospy.loginfo("RFID: %d" % (data))
 
 if __name__ == "__main__":
 	rospy.init_node("botherder")
 	bot = pathplanner.Robot()
+	rospy.Subscriber("rfidread", Int32, cb)
 
 	try:
-		rospy.loginfo("Starting in 5")
-		rospy.sleep(5)
-		while bot.trolley.y > 1:
+		rospy.loginfo("Starting in 3")
+		rospy.sleep(3)
+		"""
+		if bot.trolley.x * bot.trolley.theta < 0 and abs(bot.trolley.x > 1) :
+			rospy.loginfo("S-curve initiated")
+			if bot.trolley.x > 0:
+				bot.s_curve(bot.trolley.x + 0.5)
+
+			else:
+				bot.s_curve(bot.trolley.x - 0.5)
+		
+
+		while bot.trolley.y > 1.2:
+			bot.check_obstacle()
 			rospy.loginfo("distance: %f" % (bot.trolley.y))
 			bot.go()
 			
 		bot.stop()			
 		rospy.loginfo("begin alignment")
+		
 		bot.align()
-		bot.stop()
-		bot.gripper_up()
-		rospy.sleep(5)
-		#bot.publish_vel(-0.12,-0.12,5)
-		bot.gripper_down()
+		bot.vel_publish(0,0,3)
+
+		if bot.trolley.y < 0.1:
+			bot.gripper_up()
+			#rospy.sleep(5)
+			bot.vel_publish(-0.12,-0.12,5)
+			bot.gripper_down()
+			bot.rev_stop()
+		"""
+		while not rospy.is_shutdown():
+			pass
 
 	except rospy.ROSInterruptException:
-		pass
+		rospy.loginfo("Done")
 
 	
 		
--- ROS/trolley/src/pathplanner.py
+++ ROS/trolley/src/pathplanner.py
@@ -10,7 +10,9 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Float64
 from trolley.msg import Wheels
 
-
+"""
+All dimensions are in SI units
+"""
 
 class Coordinate(object):
 	def __init__(self, a = 0, b = 0, t = 0):
@@ -25,8 +27,9 @@ class Path_Planner(object):
 		self.timeStep = 0.2
 		self.pt1 = Coordinate()
 		self.trolley = Coordinate()
-		self.base_speed = 0.12
+		self.base_speed = 0.13 
 		self.get_dock_pose()
+		self.check_obstacle()
 		rospy.loginfo("Marker pose set")
 
 
@@ -70,8 +73,9 @@ class Path_Planner(object):
 		return(vl, vr)
 
 	def compute_control_point(self):
-		self.pt1.x = self.trolley.x / 2 
-		self.pt1.y = self.trolley.y / 2 - 0.3
+		self.pt1.x = self.trolley.x  *5/6
+		self.pt1.y = self.trolley.y * 2 
+
 		rospy.loginfo("Control point x: %f, y: %f" % (self.pt1.x, self.pt1.y))
 
 		
@@ -121,17 +125,22 @@ class Robot(Path_Planner, Gripper):
 		self.elapsed_time = 0
 		self.start_pose = Coordinate()
 		self.obstacle_flag = 0
-
+		self.reverse_flag = 0
+		
+	
 	def check_obstacle(self):
 		rospy.Subscriber("obstacle", Float64, self.obstacle_cb)
 
 	def obstacle_cb(self, msg):
+		rospy.loginfo(msg.data)
 		self.obstacle_flag = msg.data
 
 	def vel_publish(self, left_vel = 0.12, right_vel = 0.12, t = 0): #publish movements
 		pub = rospy.Publisher("/cmd_vel", Wheels, queue_size = 30)
 		msg = Wheels()
-		if int(self.obstacle_flag) == 1:
+		rospy.loginfo(self.obstacle_flag)
+		rospy.loginfo(self.reverse_flag)
+		if self.obstacle_flag == 1 and self.reverse_flag == 0:
 			rospy.loginfo("obstacle_detected, stopping")
 			msg.left = 0
 			msg.right = 0
@@ -154,43 +163,38 @@ class Robot(Path_Planner, Gripper):
 					if now - start_time > t:
 						return 0
 
-	def ekf_sub(self):
-		rospy.Subscriber("/odometry/filtered", Odometry, self.get_current_pose)
-		self.elapsed_time = time.time() - self.start_time
-		rospy.sleep(0.2)
-
-	def get_current_pose(self, msg):
-		if self.start_pose.x == 0 or self.start_pose.y == 0 or self.start_pose.theta == 0:
-			rospy.loginfo("setting initial pose")
-			self.start_pose.x = msg.pose.pose.position.x
-			self.start_pose.y = msg.pose.pose.position.y
-			(roll, ptich,yaw) = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-			self.start_pose.theta = yaw
-			rospy.loginfo("initial x: %f, initial y: %f, initial theta: %f" % (self.start_pose.x, self.start_pose.y, self.start_pose.theta))
+	def s_curve(self, d):
+		"""
+		Function takes diameter and converts it into the necessary wheel velocities to traverse a S-curve
+		"""	
+		radius = d/2
+		vl, vr = self.compute_wheel_vel(radius)
+		w = (vl-vr)/2
 		
-		self.current_pose.x = msg.pose.pose.position.x - self.start_pose.x
-		self.current_pose.y = msg.pose.pose.position.y - self.start_pose.y
-		
-		(roll, ptich,yaw) = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-		self.current_pose.theta = yaw - self.start_pose.theta
-		#rospy.loginfo("msg x: %f, msg y: %f, msg theta: %f" % (msg.pose.pose.position.x, msg.pose.pose.position.y, yaw))
-		#rospy.loginfo("current x: %f, current y: %f, current theta: %f" % (self.current_pose.x, self.current_pose.y, self.current_pose.theta))
+		#time taken to travel quarter circle
+		t = (math.pi/2)/w -1
 
+		self.vel_publish(vl,vr,t)
+		self.vel_publish(self.base_speed, self.base_speed, radius)
+		self.vel_publish(vr,vl,t)
+		self.stop()
+		
 
 	def go(self):
 		"""
 		The robot travels to the trolley by generating an arc between it and the dock
 		1. get marker pose
-		2. generate control point to set the arc
-		3. compute radius of arc
-		4. compute wheel velocities given arc radius
-		5. publish velocities
-		6. repeat
+		2. If trolley is faced away from robot traverse in s curve to shift robot laterally
+		3. generate control point to set the arc
+		4. compute radius of arc
+		5. compute wheel velocities given arc radius
+		6. publish velocities
+		7. repeat
 		"""		
 		self.compute_control_point()
 		r = self.compute_radius()
 		vl, vr = self.compute_wheel_vel(r)
-		self.vel_publish(vl, vr, 1)
+		self.vel_publish(vl, vr)
 
 
 	def align(self):
@@ -199,33 +203,33 @@ class Robot(Path_Planner, Gripper):
 		bang bang control
 		"""
 		rospy.loginfo("alignment")
-		while abs(self.trolley.theta) > 0.05 or self.trolley.y > 0.1:	
-			self.get_dock_pose()
+		while abs(self.trolley.theta) > 0.05 or self.trolley.y > 0.05:	
 			x_err = 0.2 * self.trolley.x
+			rospy.loginfo("Loop")
 			rospy.loginfo(self.trolley.theta)			
-			if self.trolley.theta > 0.3:
+			if self.trolley.theta > 0.2:
 				vl = 0.15 
-				vr = 0.24
+				vr = 0.30
 
-			elif self.trolley.theta > 0.15:
+			elif self.trolley.theta > 0.1:
 				vl = 0.15
-				vr = 0.19
+				vr = 0.22
 
-			elif self.trolley.theta > 0.05:
+			elif self.trolley.theta > 0.01:
 				vl = 0.15
-				vr = 0.18
+				vr = 0.17
 
-			elif self.trolley.theta < -0.3:
+			elif self.trolley.theta < -0.2:
 				vr = 0.15
 				vl = 0.24
 
-			elif self.trolley.theta < -0.15:
+			elif self.trolley.theta < -0.1:
 				vr = 0.15
 				vl = 0.19
 
-			elif self.trolley.theta < -0.05:
+			elif self.trolley.theta < -0.01:
 				vr = 0.15
-				vl = 0.15
+				vl = 0.17
 
 			else:
 				rospy.loginfo("angle aligned")
@@ -239,21 +243,28 @@ class Robot(Path_Planner, Gripper):
 
 				else:
 					rospy.loginfo("aligned")
-					vl = 0.15
-					vr = 0.15
+					vl = 0.2
+					vr = 0.2
 
-			self.vel_publish(vl,vr)
-		if self.trolley.y < 0.1:
 			self.vel_publish(vl,vr,2)
+			if self.trolley.y < 0.1:
+				break
+		
 
 
 	def stop(self):
-		self.vel_publish(0.13)
-		self.vel_publish(0.12)
-		self.vel_publish(0.12)
-		self.vel_publish(0.11)
-		self.vel_publish(0.11)
-		self.vel_publish(0,0,2)	
+		self.vel_publish(0.12, 0.12)
+		self.vel_publish(0.10, 0.10)
+		self.vel_publish(0.08, 0.08)
+		self.vel_publish(0.06, 0.06)
+		self.vel_publish(0,0,5)	
+
+	def rev_stop(self):
+		self.vel_publish(-0.12, -0.12)
+		self.vel_publish(-0.10, -0.10)
+		self.vel_publish(-0.08, -0.08)
+		self.vel_publish(-0.06, -0.06)
+		self.vel_publish(-0,-0,5)
 
 	def is_docked(self):
 		if abs(self.current_pose.x - self.trolley.x) < 0.3 and abs(self.current_pose.y - self.trolley.y) < 0.3 and abs(self.current_pose.theta - self.trolley.theta) < 0.1:
@@ -266,7 +277,7 @@ class Robot(Path_Planner, Gripper):
 
 
 	def get_dock_pose(self):
-		rospy.Subscriber("aruco_single/pose", PoseStamped, self.aruco_callback)
+		rospy.Subscriber("aruco_single/pose", PoseStamped, self.aruco_callback2)
 		
 
 					
@@ -276,33 +287,28 @@ class Robot(Path_Planner, Gripper):
 		quaternion = msg.pose.orientation
 		r,p,y = euler_from_quaternion((quaternion.x, quaternion.y, quaternion.z, quaternion.w))
 
-		if dist.x < 0:
-			self.trolley.x = dist.x - 0.1
+		y_val = math.sqrt(dist.z**2 - dist.x**2) 
 
-		else:
-			self.trolley.x = dist.x + 0.1
-
-		self.trolley.y = dist.z - 0.3
 		self.trolley.theta = p
-
+		self.trolley.x = dist.x + 0.62 * math.sin(p)
+		self.trolley.y = y_val - 0.62 * math.cos(p)
+		rospy.loginfo("y: %f" % (self.trolley.y))
 
 		#rospy.loginfo("roll: %f, pitch: %f yaw: %f" % (r,p,y))
 		#rospy.loginfo("trolley x: %f, trolley y: %f, trolley angle: %f" % (dist.x, dist.z, y))
-		
-		
+
+
 	def aruco_callback2(self, msg):
 		dist = msg.pose.position
 		quaternion = msg.pose.orientation
 		r,p,y = euler_from_quaternion((quaternion.x, quaternion.y, quaternion.z, quaternion.w))
 
-		if dist.x < 0:
-			self.trolley.x = dist.x - 0.1
+		y_val = math.sqrt(dist.z**2 - dist.x**2) 
 
-		else:
-			self.trolley.x = dist.x + 0.1
-
-		self.trolley.y = dist.z - 0.3
 		self.trolley.theta = p
+		self.trolley.x = dist.x + 0.62 * math.sin(p)
+		self.trolley.y = y_val - 0.62 * math.cos(p)
+		rospy.loginfo("y: %f" % (self.trolley.y))
 		
 		if self.trolley.y > 1.2:
 			self.go()
@@ -311,11 +317,11 @@ class Robot(Path_Planner, Gripper):
 			rospy.loginfo("Beginning alignment")
 			if self.trolley.theta > 0.3:
 				vl = 0.15 
-				vr = 0.3
+				vr = 0.25
 
 			elif self.trolley.theta > 0.15:
 				vl = 0.15
-				vr = 0.24
+				vr = 0.2
 
 			elif self.trolley.theta > 0.05:
 				vl = 0.15
@@ -323,28 +329,17 @@ class Robot(Path_Planner, Gripper):
 
 			elif self.trolley.theta < -0.3:
 				vr = 0.15
-				vl = 0.3
+				vl = 0.25
 
 			elif self.trolley.theta < -0.15:
 				vr = 0.15
-				vl = 0.24
+				vl = 0.2
 
 			elif self.trolley.theta < -0.05:
 				vr = 0.15
 				vl = 0.18
 
 			else:
-				rospy.loginfo("angle aligned")
-				if x_err > 0.05:
-					vl = 0.16
-					vr = 0.12
-
-				elif x_err < -0.05:
-					vl = 0.12
-					vr = 0.16
-
-				else:
-					rospy.loginfo("aligned")
 					vl = 0.15
 					vr = 0.15
 
@@ -352,4 +347,14 @@ class Robot(Path_Planner, Gripper):
 			
 		else:
 			rospy.loginfo("docked")
-		
+			self.vel_publish(0,0,1)
+			self.reverse_flag = 1
+			self.gripper_up()
+			#rospy.sleep(5)
+			self.vel_publish(-0.12,-0.12,5)
+			self.gripper_down()
+			self.rev_stop()
+			rospy.is_shutdown = True
+			self.reverse_flag = 0
+			rospy.sleep(5)
+
